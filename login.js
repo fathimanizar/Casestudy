10717cd21f5ea54dbbccfbea5d0d710710fbf627
Fathima Nizar <fathimaen@gmail.com>
2022-05-09 17:54:54 +0530

    Add decrement on uncheck

--- login.js
+++ login.js
@@ -2,11 +2,18 @@ function validate(redirect)
 {
 var uname=document.getElementById("uname").value;
 var pwd=document.getElementById("pwd").value;
+var error=document.getElementById("error");
 
     if(uname=="admin" && pwd=="12345")
         redirect();
-    else 
-        alert("Invalid username/password");       
+    else
+    {
+        error.textContent = "Please enter a valid email";
+        error.style.color = "red";
+        event.preventDefault();
+        return false;
+    } 
+        // alert("Invalid username/password");       
 }
 
 function redirect()
--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function list()
             checkbox=`<input id='box' type='checkbox' checked disabled>`
           }
            else{
-            checkbox=`<input id='box' onchange="find();" type='checkbox' >`
+            checkbox=`<input id='box' onchange="find(this);" type='checkbox' >`
            }
            output+="<table><tr><td>"+response[i].id+"&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;"+response[i].title+checkbox+"</td></tr></table";
         }
@@ -29,22 +29,42 @@ function list()
    xhttp.send();
 }
 var c=0;
-function find()
-{
-   var promise= new Promise(function(resolve,reject)
-  {
-    c++;
-    if(c==5)
-    {
-      resolve("Congrats. 5 Tasks have been Successfully Completed ");
-    }
-  });
-  promise
-  .then(function(s){
-     alert(s);
-  })
-}
+// function find()
+// {
+//    var promise= new Promise(function(resolve,reject)
+//   {
+//     c++;
+//     if(c==5)
+//     {
+//       resolve("Congrats. 5 Tasks have been Successfully Completed ");
+//     }
+//   });
+//   promise
+//   .then(function(s){
+//      alert(s);
+//   })
+// }
   
 
+function find(element) {
+  var p = new Promise (function(resolve){
+    if(element.checked) {
+      c++;
+    } else {
+      c--;
+    }
 
+    console.log(c)
+    if (c == 5) {
+      c=0;
+      resolve("Congrats. 5 Tasks have been Successfully Completed")
+    }
+  });
+
+  p
+  .then(function(s){
+    alert(s);
+  });
+
+}
          
